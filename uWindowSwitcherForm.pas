9b57f6c682d5d47d1866d0bf64fb2df70799ca0d
wookoon2024 <wookoon@gmail.com>
2026-08-17 18:11:50 +0900

    fix(build): dynamic binding for QueryFullProcessImageNameW and declare PROCESS_QUERY_LIMITED_INFO for 100% compilation

--- uWindowSwitcherForm.pas
+++ uWindowSwitcherForm.pas
@@ -612,26 +612,44 @@ begin
 end;
 
 function TWindowSwitcherForm.GetProcessFullExePath(APID: DWORD): string;
+const
+  PROCESS_QUERY_LIMITED_INFO = $1000;
+type
+  TQueryFullProcessImageNameW = function(hProcess: THandle; dwFlags: DWORD; lpExeName: PWideChar; var lpdwSize: DWORD): BOOL; stdcall;
 var
   LProcHandle: THandle;
-  LBuf: array[0..MAX_PATH] of Char;
+  LBuf: array[0..MAX_PATH] of WideChar;
   LSize: DWORD;
+  LKernel32: HMODULE;
+  LQueryFunc: TQueryFullProcessImageNameW;
 begin
   Result := '';
   if APID = 0 then Exit;
   
-  // 64비트 프로세스(Chrome, Edge 등) 및 관리자 권한 프로세스 호환
-  LProcHandle := OpenProcess(PROCESS_QUERY_LIMITED_INFORMATION, False, APID);
+  // 64비트 프로세스(Chrome, Edge 등) 및 권한 제한 프로세스 호환
+  LProcHandle := OpenProcess(PROCESS_QUERY_LIMITED_INFO, False, APID);
   if LProcHandle = 0 then
     LProcHandle := OpenProcess(PROCESS_QUERY_INFORMATION or PROCESS_VM_READ, False, APID);
     
   if LProcHandle <> 0 then
   begin
     try
-      LSize := MAX_PATH;
-      if QueryFullProcessImageName(LProcHandle, 0, LBuf, LSize) then
-        Result := string(LBuf)
-      else if GetModuleFileNameEx(LProcHandle, 0, LBuf, MAX_PATH) > 0 then
+      LKernel32 := GetModuleHandle('kernel32.dll');
+      if LKernel32 <> 0 then
+      begin
+        @LQueryFunc := GetProcAddress(LKernel32, 'QueryFullProcessImageNameW');
+        if Assigned(LQueryFunc) then
+        begin
+          LSize := MAX_PATH;
+          if LQueryFunc(LProcHandle, 0, LBuf, LSize) then
+          begin
+            Result := string(LBuf);
+            Exit;
+          end;
+        end;
+      end;
+      
+      if GetModuleFileNameEx(LProcHandle, 0, LBuf, MAX_PATH) > 0 then
         Result := string(LBuf);
     finally
       CloseHandle(LProcHandle);
